9f2b7825182715fd7d34454ccfd4e58aab4f6af2
LiSi <2323541064@qq.com>
2020-08-14 15:42:08 +0800

    修改 gitDemo

--- boot-ssm/src/main/java/com/qfjy/gitDemo.java
+++ boot-ssm/src/main/java/com/qfjy/gitDemo.java
@@ -4,4 +4,10 @@ public class gitDemo {
     public static void main (String[] args){
         System.out.println("Hello Git。。。");
     }
+
+    public  void test(){
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
